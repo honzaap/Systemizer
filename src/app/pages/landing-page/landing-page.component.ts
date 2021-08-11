44dc789439aefbfa878c539ff05cc26ae820b3df
honzaap <paneja2@seznam.cz>
2021-08-11 18:01:19 +0200

    Adding supporters&contributors to landing page

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -7,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LandingPageComponent implements OnInit {
 
-	constructor() { }
+	supporters: any[] = [];
+	contributors: any[] = [];
+	constructor() { 
+		// Supporters are being added manually since buymeacoffee API leaks e-mail adress/sensitive info and I have no backend to process it for me :/
+		this.supporters.push({name: "Sean Cline", amount: "15", imgUrl: "https://img.buymeacoffee.com/api/?name=Sean+Cline&size=300&bg-image=bmc&background=FF813F"});
+	
+		// Get contributors from GitHub API
+		try{
+			fetch("https://api.github.com/repos/honzaap/systemizer/contributors")
+			.then(res => res.json().then(data => {
+				if(!Array.isArray(data))
+					return;
+				data.forEach(contributor => {
+					this.contributors.push({
+						name: contributor.login,
+						imgUrl: contributor.avatar_url,
+						link: contributor.html_url
+					})
+				})
+			}))
+		}
+		catch{ }
+	}
 
 	ngOnInit(): void {
 		document.getElementsByClassName("page")[0].scrollTop = 0;
 	}
 
+	scaleFontSize(name){
+		return Math.min(240/name.length, 19) + 'px';
+	}
 }
